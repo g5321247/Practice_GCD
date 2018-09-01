076b7317cb5a970fd700bb203798bf755bc2748e
GeorgeLIu <g5321247@gmail.com>
2018-09-01 16:50:04 +0800

    Parse data from server successfully

--- GCD/GCD/ViewController.swift
+++ GCD/GCD/ViewController.swift
@@ -18,9 +18,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
-    
+        getData(name: "name")
+        getData(name: "address")
+        getData(name: "head")
+        
+//        tableView.delegate = self
+//        tableView.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,14 +34,15 @@ class ViewController: UIViewController {
     
     func getData(name: String) {
         
-        guard let url = URL(string: "https://stations-98a59.firebaseio.com/\(name).json") else { print("url error") }
+        guard let url = URL(string: "https://stations-98a59.firebaseio.com/\(name).json") else { return print("url error") }
         
-        let urlRequest = URLRequest(url: url)
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
         let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
             
             guard error == nil else { return print("error is not nil") }
             
-            guard let data = data else { return print("data error") }
+            guard let data = data  else { return print("data error") }
             
             guard let response = response as? HTTPURLResponse else { return print("data error") }
             
@@ -45,25 +50,26 @@ class ViewController: UIViewController {
                 
             case 200 ... 299:
                 
-                self.onlineData.append((data as? String)!)
+                let result = String(decoding: data, as: UTF8.self)
+                self.onlineData.append(result)
                 
             default:
                 print("errorCode: \(response.statusCode)")
             }
             
         }
-        
+        dataTask.resume()
     }
 
-    
+
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return
-    }
-    
-}
+//extension ViewController: UITableViewDelegate, UITableViewDataSource {
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return
+//    }
+//
+//}
 
 
