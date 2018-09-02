5abfaf2e63364a328c4c31b04c16070633fa8ec9
GeorgeLIu <g5321247@gmail.com>
2018-09-02 16:09:40 +0800

    Add dispatchGroup in completion handler

--- GCD/GCD/ViewController.swift
+++ GCD/GCD/ViewController.swift
@@ -8,68 +8,102 @@
 
 import UIKit
 
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var textLbl: UILabel!
+
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var websiteName = ["name", "address", "head"]
+    let dispatchGroup = DispatchGroup()
+
+    let websiteName = ["name", "address", "head"]
+    
     var onlineData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getData(name: "name")
-        getData(name: "address")
-        getData(name: "head")
+        getData()
+
+        tableView.delegate = self
+        tableView.dataSource = self
         
-//        tableView.delegate = self
-//        tableView.dataSource = self
-        
+        dispatchGroup.notify(queue: .main) {
+            
+            self.tableView.reloadData()
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
     
-    func getData(name: String) {
+    func getData() {
         
-        guard let url = URL(string: "https://stations-98a59.firebaseio.com/\(name).json") else { return print("url error") }
-        
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "GET"
-        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+        for address in websiteName {
             
-            guard error == nil else { return print("error is not nil") }
+            guard let url = URL(string: "https://stations-98a59.firebaseio.com/\(address).json") else { return print("url error") }
             
-            guard let data = data  else { return print("data error") }
+            let urlRequest = URLRequest(url: url)
             
-            guard let response = response as? HTTPURLResponse else { return print("data error") }
-            
-            switch response.statusCode {
+            dispatchGroup.enter()
+
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
                 
-            case 200 ... 299:
-                
-                let result = String(decoding: data, as: UTF8.self)
-                self.onlineData.append(result)
-                
-            default:
-                print("errorCode: \(response.statusCode)")
+                DispatchQueue.main.async {
+                    
+                    guard error == nil else { return print("error is not nil") }
+                    
+                    guard let data = data  else { return print("data error") }
+                    
+                    guard let response = response as? HTTPURLResponse else { return print("data error") }
+                    
+                    switch response.statusCode {
+                        
+                    case 200 ... 299:
+                        
+                        let result = String(decoding: data, as: UTF8.self)
+                        self.onlineData.append(result)
+                        
+                    default:
+                        print("errorCode: \(response.statusCode)")
+                    }
+                    self.dispatchGroup.leave()
+                }
             }
             
+            dataTask.resume()
         }
-        dataTask.resume()
+        
     }
-
 
 }
 
-//extension ViewController: UITableViewDelegate, UITableViewDataSource {
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return
-//    }
-//
-//}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return onlineData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CustomCell else {
+            print("No such cell")
+            return UITableViewCell()
+        }
+        
+       
+            
+            cell.textLbl.text = self.onlineData[indexPath.row]
+            cell.textLbl.isHidden = false
+        
+        return cell
+    }
+}
 
 
